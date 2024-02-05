c9d63e09ae39c6dffad1e092d279b7f33c141ed1
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-02-05 16:52:01 +0000

    feat: Fix formatting, add devDependencies, add imports and create Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
