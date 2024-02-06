0f935ee161347cd5b2a454839b4478aa17ab910e
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-02-06 09:33:52 +0000

    feat: Add tailwindcss v3.4.1 dependency, update Navbar component styling, add App component, add index.css and render App component, add tailwind.config.js and add tsconfig.json

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 function Navbar() {
   return (
     <header>
-      <div className="flex items-center justify-between p-4">
+      <div className="">
         <Link to="/">
           <h1 className="text-5xl">Chrono MD</h1>
         </Link>
