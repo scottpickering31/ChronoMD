3376cf40182281057074b5520c8d10331aae2d5d
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-02-15 16:09:19 +0000

    feat: Refactor CSS classes, rename sidebar headings and fix typo

--- frontend/src/containers/Sidebar.tsx
+++ frontend/src/containers/Sidebar.tsx
@@ -2,29 +2,27 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="w-tiny absolute overflow-y-scroll top-0 -z-10 left-0 h-screen bg-slate-100 flex justify-evenly flex-col text-center">
+    <div className="w-tiny absolute top-0 -z-10 left-0 h-screen bg-slate-100 flex justify-evenly flex-col text-center">
       <div>
-        <h1>Setting1Header</h1>
+        <h1>Work Planner</h1>
+        <h2>Setting1</h2>
+      </div>
+      <div>
+        <h1>Learning Platform</h1>
+        <h2>Setting1</h2>
+      </div>
+      <div>
+        <h1>Clinical Practice</h1>
         <h2>Setting1</h2>
         <h2>Setting2</h2>
       </div>
       <div>
-        <h1>Setting2Header</h1>
+        <h1>Medical Journals</h1>
         <h2>Setting1</h2>
         <h2>Setting2</h2>
       </div>
       <div>
-        <h1>Setting3Header</h1>
-        <h2>Setting1</h2>
-        <h2>Setting2</h2>
-      </div>
-      <div>
-        <h1>Setting4Header</h1>
-        <h2>Setting1</h2>
-        <h2>Setting2</h2>
-      </div>
-      <div>
-        <h1>Setting5Header</h1>
+        <h1>Library</h1>
         <h2>Setting1</h2>
         <h2>Setting2</h2>
       </div>
