df2afec6adbc824d51a822193d50aed0a7b9ac64
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-02-21 16:53:21 +0000

    feat: Add conditional rendering logic based on user login status in NavSearchBar component

--- frontend/src/components/navbar/searchbar/NavSearchBar.tsx
+++ frontend/src/components/navbar/searchbar/NavSearchBar.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import MagnifyingGlass from "../searchbar/MagnifyingGlass";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../state/store/store";
 
 function NavSearchBar() {
+  const loggedIn = useSelector((state: RootState) => state.loggedIn.value);
+
   return (
-    <div className="flex flex-row">
-      <div className="mr-2">
-        <MagnifyingGlass />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="rounded-full p-2 w-80"
-        />
-      </div>
-    </div>
+<>
+      {loggedIn ? (
+        <div className="flex flex-row">
+          <div className="mr-2">
+            <MagnifyingGlass />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="rounded-full p-2 w-80"
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Not logged in stuff</h1>
+        </div>
+      )}
+</>
   );
 }
 
